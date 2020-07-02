4ec060a3e6f9e5d9c5a8e5612ef2f52459440613
Augusto Maza <debianmaza@gmail.com>
2020-07-02 11:41:50 -0300

    Update root's main #3

--- root/main.php
+++ root/main.php
@@ -100,12 +100,12 @@
     </div>
         
 <div class="list-group">
-  <a href="organismos/organismos.php" class="list-group-item"><span class="pull-center "><img src="../icons/actions/view-bank.png"  class="img-reponsive img-rounded"> Cargar Organismos</a>
+  <a href="organismos/organismos.php" class="list-group-item"><span class="pull-center "><img src="../icons/actions/view-bank.png"  class="img-reponsive img-rounded"> Alta de Organismos</a>
   <a href="upload_lote/lotes.php" class="list-group-item"><span class="pull-center "><img src="../icons/places/server-database.png"  class="img-reponsive img-rounded"> Lotes Subidos</a>
   <a href="upload_lote/lotes_ok.php" class="list-group-item"><span class="pull-center "><img src="../icons/places/server-database.png"  class="img-reponsive img-rounded"> Lotes Generados Manualmente</a>
   <a href="procesar_lotes/procesar_lote.php" class="list-group-item"><span class="pull-center "><img src="../icons/actions/games-solve.png"  class="img-reponsive img-rounded"> Procesar Lotes Subidos</a>
   <a href="liquidadores/liquidadores.php" class="list-group-item"><span class="pull-center "><img src="../icons/actions/meeting-attending.png"  class="img-reponsive img-rounded"> Datos Liquidadores</a>
-  <a href="users/users.php" class="list-group-item"><span class="pull-center "><img src="../icons/actions/user-group-properties.png"  class="img-reponsive img-rounded"> Usuarios</a>
+  <a href="users/users.php" class="list-group-item"><span class="pull-center "><img src="../icons/actions/user-group-properties.png"  class="img-reponsive img-rounded"> Administración de Usuarios</a>
   </div>
   
    <div class="panel-group">
